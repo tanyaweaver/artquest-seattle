68a27ea0ca02095849234e2ae2e3e24906a68e4e
tanyaweaver <tanya83@uw.edu>
2016-05-30 11:53:53 -0700

    google map is rendering to the page

--- scripts/models/map.js
+++ scripts/models/map.js
@@ -1,7 +1,12 @@
 (function(module) {
   var mapOptions = {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 47.618217, lng: -122.351832},
+    zoom: 16
   };
-  var map = new google.maps.Map(getElementById('map'), mapOptions);
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  google.maps.event.addDomListener(window, 'resize', function() {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(center);
+  });
 })();
