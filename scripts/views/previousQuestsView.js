d2f20851f60b01089d951b5cc77b16fdbc673495
tanyaweaver <tanya83@uw.edu>
2016-06-02 09:43:24 -0700

    list shows fro each previous quest

--- scripts/views/previousQuestsView.js
+++ scripts/views/previousQuestsView.js
@@ -4,11 +4,13 @@
     $('#previous-quests').delegate('li', 'click', function() {
       var i = $('#previous-quests > li').index(this);
       console.log(i);
+      // $(this).addClass('current');
       mapView.renderMap(Quest.all[i].list);
       var template = Handlebars.compile($('#render-list-for-quest').html());
       console.log(Quest.all[i].list);
       Quest.all[i].list.forEach(function(location) {
-        $('#list-quest').append(template(location));
+        $('#' + i + ' > ul').append(template(location));
+        console.log('ha');
       });
     });
   };
