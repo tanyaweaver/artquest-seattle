8cc143e7664bbd728cf59319a7f10f849f780cb0
Derek Graham <derekgraham@mac.com>
2016-06-02 19:30:11 -0700

    fixed firebase deployment, and home controller

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -1,8 +1,17 @@
 (function(module) {
   var homeController = {};
+
   homeController.index = function() {
-    $('#home-page').show().siblings().hide();
-    $('#map-view').show();
+    console.log('homecontroller');
+    var user = firebase.auth().currentUser;
+    if (user === null) {
+      console.log('signed out');
+      $('#home-page').show().siblings().hide();
+    } else {
+      console.log('signedIN');
+      page('/signedin');
+
+    }
   };
   module.homeController = homeController;
 })(window);
