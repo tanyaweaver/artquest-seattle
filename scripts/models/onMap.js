f3e7322dde4144658873aacf5eb1ef2f3ab58156
tanyaweaver <tanya83@uw.edu>
2016-06-03 12:58:26 -0700

    cleaned up onMap.js

--- scripts/models/onMap.js
+++ scripts/models/onMap.js
@@ -33,7 +33,6 @@
       });
       onMap.markersArray.push(marker);
 
-      //each marker has an info window on click, displaying title
       google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
           infoWindow.setContent(locationsArray[i].title);
@@ -54,16 +53,13 @@
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-        console.log(map.getCenter() + 'before setCenter(pos)');
         map.setCenter(pos);
-        console.log(map.getCenter() + 'after setCenter(pos)');
         infoWindow.setPosition(pos);
         infoWindow.setContent('You are here');
       }, function() {
         handleLocationError(true, infoWindow, map.getCenter());
       });
     } else {
-      //Browser doesn't support geolocation
       handleLocationError(true, infoWindow, map.getCenter());
     }
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
